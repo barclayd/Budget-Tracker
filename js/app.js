d68f0f9b229d24ab790c2f0fef5b084d2046edfa
Daniel Barclay <barclaysd@me.com>
2019-02-02 21:17:42 +0000

    Logic to handle submitting of budget and calculating balance

--- js/app.js
+++ js/app.js
@@ -15,4 +15,72 @@ class UI {
     this.itemList = [];
     this.itemID = 0;
   }
+
+  submitBudgetForm() {
+    console.log('hello');
+    const value = this.budgetInput.value;
+    if (value < 0.01) {
+      this.budgetFeedback.classList.add('showItem');
+      this.budgetFeedback.innerHTML = `<p> Budget value cannot be negative, empty or 0 </p> `
+      setTimeout(() => {
+        this.budgetFeedback.classList.remove('showItem');
+      }, 2000);
+    } else {
+      this.budgetAmount.textContent = value;
+      this.budgetInput.value = '';
+      this.showBalance();
+    }
+  };
+
+  showBalance() {
+    console.log('Balance was shown');
+    const expenses = this.totalExpenses();
+    const total = parseFloat(this.budgetAmount.textContent) - expenses;
+    this.balanceAmount.textContent = total;
+    if (total < 0) {
+      this.balanceAmount.classList.remove('showGreen', 'showBlack');
+      this.balanceAmount.classList.add('showRed');
+    } else if (total === 0) {
+      this.balanceAmount.classList.remove('showGreen', 'showRed');
+      this.balanceAmount.classList.add('showBlack');
+    } else {
+      this.balanceAmount.classList.remove('showBlack', 'showRed');
+      this.balanceAmount.classList.add('showGreen');
+    }
+  };
+
+  totalExpenses() {
+    let total = 400;
+    return total;
+  };
+
 }
+
+const eventListeners = () => {
+  const budgetForm = document.getElementById('budget-form');
+  const expenseForm = document.getElementById('expense-form');
+  const expenseList = document.getElementById('expense-list');
+
+  const ui = new UI();
+
+  // budget form submit
+  budgetForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log(event);
+    ui.submitBudgetForm();
+  });
+
+  expenseForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log(event);
+  });
+
+  expenseList.addEventListener('click', () => {
+    console.log(event);
+  });
+
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  eventListeners();
+});
